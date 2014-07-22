defa52d68dca6b5b8f63edf4516db053beca95f2
Aamir Jawaid <lordwizards@gmail.com>
2014-07-22 03:27:12 -0400

    Initial Commit

---
+++ WordPredictor/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WordPredictor
+//
+//  Created by Aamir Jawaid on 2014-07-22.
+//  Copyright (c) 2014 Aamir Jawaid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
